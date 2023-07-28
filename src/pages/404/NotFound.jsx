73b3e76431436b6d3dfd96c7881e78e274af23fb
SaanviSingh <saanvisinghbalhara@gmail.com>
2023-07-29 02:02:52 +0530

    Commit 2

--- src/pages/404/NotFound.jsx
+++ src/pages/404/NotFound.jsx
@@ -3,8 +3,11 @@ import "./style.scss";
 
 function NotFound() {
   return (
-    <div>
-      
+    <div className="pageNotFound">
+    <ContentWrapper>
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!</span>
+    </ContentWrapper>
     </div>
   )
 }
